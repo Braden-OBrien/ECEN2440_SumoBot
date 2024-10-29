e58a72c75aa43f623827a9a26f8d0b2fc9c4434c
Braden-OBrien <120068495+Braden-OBrien@users.noreply.github.com>
2024-10-29 13:03:31 -0600

    ir rf switch implemented

--- integration_testing/lab15_integration/pt10/main.py
+++ integration_testing/lab15_integration/pt10/main.py
@@ -1,33 +1,75 @@
-import machine
-from machine import Pin, Timer
+from machine import Pin
+from ir_rx.nec import NEC_8 # Use the NEC 8-bit class
+from ir_rx.print_error import print_error # for debugging
 import time
 
 
 time.sleep(0.1)
 
-inputs = [Pin(i, Pin.IN) for i in [11, 12, 15, 14]]
+#Global constants
+debounce_time = 100 #50ms debounce timer
+
+#Assigning Pinouts
+rf_inputs = [Pin(i, Pin.IN) for i in [11, 12, 15, 14]]
+ir_input = Pin(13, Pin.IN, Pin.PULL_UP)
+input_toggle_btn = Pin(10, Pin.IN, Pin.PULL_DOWN)
 leds = [Pin(i, Pin.OUT) for i in [21, 18, 19, 20]]
 
-interrupt_flags = [0 for i in range(len(inputs))]
 
-def callback(input):
+#Interrupt flags
+led_interrupt_flags = [0 for i in range(len(rf_inputs))]
+input_toggle = 1 # 1 - RF Receiver | 0 - IR Receiver
+
+#Defining ISRs
+def rf_ISR(input):
     global interrupt_flags
     global inputs
     
-    index = inputs.index(input)
-    print('input received on', index)
-    interrupt_flags[index] = 1
+    index = rf_inputs.index(input)
+    print('rf input received on', index)
+    led_interrupt_flags[index] = 1
+    
+def ir_ISR(data, addr_, _):
+    global led_interrupt_flags
+    
+    if addr_ > range(len(rf_inputs)):
+        return
+    else:
+        print('ir input received on', addr_, 'with data', data)
+        led_interrupt_flags[int(addr_)] = data
 
+debounce = 0
+def input_toggle_ISR(btn):
+    global input_toggle
+    global debounce
+    
+    if (time.ticks_ms() - debounce > debounce_time):
+        input_toggle = not input_toggle
+        print('input toggle is now', input_toggle)
+        debounce = time.ticks_ms()
 
-for input in inputs:
-    input.irq(trigger=input.IRQ_RISING, handler=callback)
+#Assigning IRQs
+for input in rf_inputs:
+    input.irq(trigger=input.IRQ_RISING, handler=rf_ISR)
+    
+ir_receiver = NEC_8(ir_input, callback=ir_ISR)
 
+input_toggle_btn.irq(trigger=input_toggle_btn.IRQ_FALLING, handler=input_toggle_ISR)
 
+#Main loop
 while True:
-    if sum(interrupt_flags) != 0:
-        for i in range(len(interrupt_flags)):
-            if interrupt_flags[i] == 1:
-                print('toggling led on', i)
-                leds[i].toggle()
-                interrupt_flags[i] = 0
+    if sum(led_interrupt_flags) != 0:
+        for i in range(len(led_interrupt_flags)):
+            if led_interrupt_flags[i] != 0:
+                if input_toggle:
+                    #RF handler
+                    print('toggling led on', i)
+                    leds[i].toggle()
+                    led_interrupt_flags[i] = 0
+                else:
+                    #IR handler
+                    print('toggling led on', i)
+                    print('ir data is', led_interrupt_flags[i])
+                    leds[i].toggle()
+                    led_interrupt_flags[i] = 0
     continue
