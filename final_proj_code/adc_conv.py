1afcf36ca849799ab22533d032989fabae873e0e
Braden-OBrien <120068495+Braden-OBrien@users.noreply.github.com>
2024-11-21 11:37:53 -0700

    small ADC converter change

--- final_proj_code/adc_conv.py
+++ final_proj_code/adc_conv.py
@@ -1,6 +1,14 @@
 import time
 
-def sample_battery(bat_sample_time, pin, ref_pin=None, adc_ref=3.3, vol_div=3):
+def sample_battery(bat_sample_time, pin, ref_pin=None, adc_ref=3.3, vol_div=3, phys_offset=0.2423):
+    """
+    Samples the battery for specified time at the specified ADC pin:
+        bat_sample_time: time (in ms) to sample the battery
+        pin: ADC pin input for sampling
+        adc_ref: the voltage value connected to ADC_REF pin. 3.3V by default
+        vol_div: the voltage divider multiplier. In our case we have our battery voltage divided to a third for sampling
+        phys_offset: the offset by the physical system. This is a measured value dependent on resistor values/gnd offset
+    """
     bat_samples = []
     #ref_samples = []
     start_time = time.ticks_ms()
--- final_proj_code/adc_conv_test.py
+++ final_proj_code/adc_conv_test.py
@@ -6,10 +6,10 @@ time.sleep(0.1)
 
 bat_pin_in = ADC(Pin(26, Pin.IN))
 #gnd_pin_in = ADC(Pin(27, Pin.IN))
-#ref_pin_in = ADC(Pin(27, Pin.IN))
+#ref_pin_in = ADC(Pin(28, Pin.IN))
 
 while True:
-    time.sleep(5)
+    time.sleep(1)
     print('current battery sample is', adc_conv.sample_battery(bat_sample_time=0.1, pin=bat_pin_in))
     #print('current ref sample is', adc_conv.sample_battery(bat_sample_time=0.1, pin=ref_pin_in))
     #print('current gnd sample is', adc_conv.sample_battery(bat_sample_time=0.1, pin=gnd_pin_in))
